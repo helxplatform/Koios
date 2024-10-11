5752c029eef44e68d5e7fd19ad35013ccf51ed5d
ykale <14827177+yskale@users.noreply.github.com>
2024-10-11 15:02:03 -0400

    fixed the concurrency

--- src/kgx_chain.py
+++ src/kgx_chain.py
@@ -32,12 +32,7 @@ from redis.commands.graph import Graph
 logging.basicConfig(level=logging.INFO)
 
 
-
-
-
 app = FastAPI()
-
-
 
 class Question(BaseModel):
     input: str
@@ -58,14 +53,12 @@ ANSWER_PROMPT = ChatPromptTemplate.from_messages(
 
 DEFAULT_DOCUMENT_PROMPT = PromptTemplate.from_template("{page_content}")
 
-
 def _format_chat_history(chat_history: List[Tuple[str, str]]) -> List:
     buffer = []
     for human, ai in chat_history:
         buffer.append(HumanMessage(content=human))
         buffer.append(AIMessage(content=ai))
     return buffer
-
 
 # Retrieve studies based on matching concepts
 async def retrieve_studies(concepts):
@@ -78,7 +71,7 @@ async def retrieve_studies(concepts):
 
     redis_graph = Graph(redis_client, config.REDIS_GRAPH_NAME)
 
-    #Get identifier for all concepts
+    # Get identifier for all concepts
     def get_concept_identifier(concepts):
         concept_mapping = {}
         for concept in concepts:
@@ -93,23 +86,19 @@ async def retrieve_studies(concepts):
                 concept_mapping[concept] = response[0]['curie']
         return concept_mapping
 
-
-
     async def concept_details_kgx(concept_id):
         query = f"""
             MATCH (c {{id: "{concept_id}"}})-[r1]->(v:`biolink.StudyVariable`)-[r2]->(s:`biolink.Study`)
             RETURN c.name AS concept_name,v.name AS variable_name,v.id AS variable_id,v.description AS variable_desc, s.id AS study_id
             LIMIT 100
         """
-
         result = redis_graph.query(query, read_only=True)
         if result.result_set is None:
-            return pd.DataFrame(columns=['concept_name', 'variable_name','variable_id','variable_desc','study_id'])
+            return pd.DataFrame(columns=['concept_name', 'variable_name', 'variable_id', 'variable_desc', 'study_id'])
         data = result.result_set
-        df = pd.DataFrame(data, columns=['concept_name', 'variable_name','variable_id','variable_desc','study_id'])
-        df['study_id'] = df['study_id'].apply(lambda x: x.split('.')[0])  
+        df = pd.DataFrame(data, columns=['concept_name', 'variable_name', 'variable_id', 'variable_desc', 'study_id'])
+        df['study_id'] = df['study_id'].apply(lambda x: x.split('.')[0])
         return df
-
 
     async def get_study_details(concept_ids):
         dfs = []
@@ -120,7 +109,7 @@ async def retrieve_studies(concepts):
         if not len(final_df):
             return ""
         df_summary = final_df.groupby('study_id').agg(
-            concept_list=('concept_name', lambda x: ', '.join(x)),#.join(sorted(set(x)))),
+            concept_list=('concept_name', lambda x: ', '.join(x)),
             variable_desc_list=('variable_desc', lambda x: ', '.join(x)),
             variable_name_list=('variable_name', lambda x: ', '.join(x)),
             variable_id_list=('variable_id', lambda x: ', '.join(x)),
@@ -131,10 +120,9 @@ async def retrieve_studies(concepts):
             lambda x: ', '.join(set([vid.split('.')[0] for vid in x.split(', ')]))
         )
 
-      
         def get_study_data(study_id):
             if study_id != None:
-                with open(config.STUDIES_JSON_FILE) as stream:  # Replace with the path to your studies JSON file
+                with open(config.STUDIES_JSON_FILE) as stream:
                     data = json.load(stream)
                 for study in data:
                     if study['StudyId'].split('.')[0] == study_id.split('.')[0]:
@@ -143,16 +131,12 @@ async def retrieve_studies(concepts):
                             "permalink": study['Permalink'],
                             "description": study['Description'],
                         }
-
             return {"study_name": "", "permalink": "", "description": ""}
 
         df_summary[['study_name', 'permalink', 'description']] = df_summary['study_id'].apply(
             lambda x: pd.Series(get_study_data(x)))
-        # Filter out rows where study_name is empty (i.e., study not found in the JSON file)
         df_summary = df_summary[df_summary['study_name'] != ""]
 
-
-        # Concatenate the `variable_name`, `variable_id`, and `variable_desc`
         df_summary['variable_info'] = df_summary.apply(
             lambda row: '\n'.join([f"\t {name} ({var_id}): {desc}"
                                 for name, var_id, desc in zip(
@@ -161,8 +145,7 @@ async def retrieve_studies(concepts):
                                     row['variable_desc_list'].split(', ')
                                 )]), axis=1)
 
-        # Display the specific columns
-        df_display = df_summary[['study_id', 'description', 'variable_info','number_of_concepts']]
+        df_display = df_summary[['study_id', 'description', 'variable_info', 'number_of_concepts']]
         top_studies = df_display.head(10)
 
         documents = []
@@ -170,7 +153,6 @@ async def retrieve_studies(concepts):
         for _, row in top_studies.iterrows():
             study_data = get_study_data(row['study_id'])
             if study_data:
-                # Create the document in dictionary format (JSON serializable)
                 doc = {
                     "page_content": f"{row['description']}\n\n Variable_info: \n{row['variable_info']}",
                     "metadata": {
@@ -186,17 +168,12 @@ async def retrieve_studies(concepts):
             for doc in documents
         ]
         return "\n".join(docs_str)
-    
-
 
     concept_ids = get_concept_identifier(concepts)
     if not concept_ids:
-        return "No information available"  # Handle no biomedical concepts case
+        return "No information available"
     study_docs_str = await get_study_details(concept_ids)
     return study_docs_str
-
-
-
 
 # Initialize the chain for concept
 def init_concept_chain():
@@ -206,7 +183,6 @@ def init_concept_chain():
             base_url=config.LLM_URL,
             model=config.GEN_MODEL_NAME
         )
-
     elif config.LLM_SERVER_TYPE == "OLLAMA":
         llm = Ollama(
             base_url=config.LLM_URL,
@@ -215,42 +191,31 @@ def init_concept_chain():
     else:
         raise ValueError(f"Invalid LLM Server type {config.LLM_SERVER_TYPE}")
 
+    get_studies_and_variables = RunnableLambda(func=lambda x: x, afunc=retrieve_studies, name="retrieve_studies_from_kg")
 
-
-    get_studies_and_variables = RunnableLambda(func=lambda x: x, afunc=retrieve_studies, name="retrieve_studies_from_kg") 
-
-
-    # extract concept
     concept_extraction_prompt = config.langfuse.get_prompt("CONCEPT_EXTRACTION_PROMPT").prompt
-    ce_prompt = ChatPromptTemplate.from_messages([('system',concept_extraction_prompt)])
+    ce_prompt = ChatPromptTemplate.from_messages([('system', concept_extraction_prompt)])
     extract_concepts = ce_prompt | llm | StrOutputParser()
-
 
     _inputs = RunnableParallel(
         {
             "input": lambda x: x["input"],
-
             "chat_history": lambda x: _format_chat_history(x['chat_history']),
             "context": extract_concepts | get_studies_and_variables | StrOutputParser()
-
-
         }
     )
 
     answer_generation_chain = RunnableBranch(
-        # check if we can get some studies from the graph.
         (
             RunnableLambda(lambda x: print(x) or bool(x.get("context"))).with_config(
                 run_name="has_context"
             ),
             ANSWER_PROMPT | llm | StrOutputParser()
         ),
-        # If no studies from the graph, and empty context respond with static text
         RunnableLambda(lambda x: "No studies were found to answer the query."),
     )
 
     qachain = _inputs | answer_generation_chain
-
     qachain = config.configure_langfuse(qachain)
 
     return qachain
@@ -265,6 +230,3 @@ add_routes(
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="localhost", port=8000, log_level="debug")
-
-
-
